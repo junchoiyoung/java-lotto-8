1d9233d962c05f65d779a6fe6c347386e82c7dad
최준영 <cjy4780@naver.com>
2025-11-03 02:03:59 +0900

    feat: 중복 검증과 숫자 범위 검증 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,13 +1,23 @@
 package lotto;
 
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import lotto.validate.Validate;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplicate(numbers);
+        validateNumberRange(numbers);
         this.numbers = numbers;
+    }
+
+    public List<Integer> getNumbers() {
+        return Collections.unmodifiableList(numbers);
     }
 
     private void validate(List<Integer> numbers) {
@@ -16,5 +26,17 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> numberSet = new HashSet<>(numbers);
+
+        if (numbers.size() != numberSet.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복 불가합니다.");
+        }
+    }
+
+    private void validateNumberRange(List<Integer> numbers) {
+        for (Integer i : numbers) {
+            Validate.numberRange(i);
+        }
+    }
 }
