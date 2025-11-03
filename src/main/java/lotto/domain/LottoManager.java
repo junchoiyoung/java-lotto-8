1b302828c59c89f722a9b5a6fd19e1d28f4fcce8
최준영 <cjy4780@naver.com>
2025-11-03 13:27:32 +0900

    feat: 생성자 파라미터 변경 및 get메소드 생성

--- src/main/java/lotto/domain/LottoManager.java
+++ src/main/java/lotto/domain/LottoManager.java
@@ -1,13 +1,24 @@
 package lotto.domain;
 
 import java.util.List;
+import java.util.Map;
 import lotto.Lotto;
 
 public class LottoManager {
+    private final LottoMaker lottoMaker;
     private final LottoMatchChecker lottoMatchChecker;
 
-    public LottoManager(int price, List<Integer> winNumber, int bonusNum) {
-        List<Lotto> lottos = new LottoMaker().makeLotto(price);
-        lottoMatchChecker = new LottoMatchChecker(lottos, winNumber, bonusNum);
+    public LottoManager(LottoMaker lottoMaker, LottoMatchChecker lottoMatchChecker) {
+        this.lottoMaker = lottoMaker;
+        this.lottoMatchChecker = lottoMatchChecker;
+    }
+
+    public List<Lotto> getLottos(int price) {
+        return lottoMaker.makeLotto(price);
+    }
+
+    public Map<Grade, Integer> getResult(List<Lotto> lottos, List<Integer> winNumber, int bonusNum) {
+        lottoMatchChecker.calculate(lottos, winNumber, bonusNum);
+        return lottoMatchChecker.getGradeCount();
     }
 }
